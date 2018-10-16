4423100e89f0ab9e235e8bf2f4de77088f8ef626
kapil <kapilrawal1995@gmail.com>
2018-10-16 02:17:57 -0400

    [#11 : LLDS] Implemented all methods of Lane Line Detection class

---
+++ include/LaneLineDetectionSystem.h
@@ -0,0 +1,63 @@
+/**
+ *  @file    LaneLineDetectionSystem.h
+ *
+ *  @author  Kapil Rawal(Driver)
+ *           Hrishikesh Tawade(Navigator)
+ *
+ *  @copyright 3 Clause BSD License
+ *
+ *  @brief ENPM808X, Mid semester project
+ *
+ *  @section DESCRIPTION
+ *
+ *  This program detects the lane line from an image or video
+ *
+ */
+
+#ifndef INCLUDE_LANELINEDETECTIONSYSTEM_H_
+#define INCLUDE_LANELINEDETECTIONSYSTEM_H_
+
+#include <string>
+#include <iostream>
+#include "../include/CameraModule.h"
+#include "../include/ImageProcessingModule.h"
+#include "../include/DriveHeadingModule.h"
+
+/**
+ *  @brief   Class for camera module.
+ */
+class LaneLineDetectionSystem {
+ public:
+  /**
+   *  @brief   Constructor
+   */
+  LaneLineDetectionSystem();
+
+  /**
+   *  @brief   Destroys the object
+   */
+  ~LaneLineDetectionSystem();
+
+  /**
+   *  @brief  Detects lane line in an image of a car driving on
+   *          a highway
+   *
+   *  @param  path to the image
+   */
+  void detectLaneLineForImage(const std::string &);
+
+  /**
+   *  @brief  Detects lane line in a video of a car driving on
+   *          a highway
+   *
+   *  @param  path to the video
+   */
+  void detectLaneLineForVideo(const std::string &);
+
+ private:
+  CameraModule camera_;
+  ImageProcessingModule imageProcessor_;
+  DriveHeadingModule driveHeadingCalculator_;
+};
+
+#endif  //  INCLUDE_LANELINEDETECTIONSYSTEM_H_
