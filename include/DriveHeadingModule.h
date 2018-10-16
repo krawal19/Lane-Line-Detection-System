002b1f221ccc2d2a6e681167adc5441895c370fe
kapil <kapilrawal1995@gmail.com>
2018-10-16 02:00:36 -0400

    [#10 : DHM] implemented all methods of drive heading module

--- include/DriveHeadingModule.h
+++ include/DriveHeadingModule.h
@@ -1,8 +1,8 @@
 /**
  *  @file    DriveHeadingModule.h
  *
- *  @author  Hrishikesh Tawade(Driver)
- *           Kapil Rawal(Navigator)
+ *  @author  Kapil Rawal(Driver)
+ *           Hrishikesh Tawade(Navigator)
  *
  *  @copyright 3 Clause BSD License
  *
@@ -13,24 +13,22 @@
  *  This program calculates the drive heading
  */
 
-#ifndef Drive_Heading_H
-#define Drive_Heading_H
+#ifndef INCLUDE_DRIVEHEADINGMODULE_H_
+#define INCLUDE_DRIVEHEADINGMODULE_H_
 
-#include <vector>
 #include <math.h>
 #include <iostream>
-#include <opencv2/opencv.hpp>
-#include <opencv2/highgui.hpp>
+#include <vector>
 #include <array>
 #include <string>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
 
 /**
  *  @brief   Class for DriveHeading
  */
 class DriveHeadingModule{
-
  public:
-
   /**
    *  @brief   Constructor
    */
@@ -46,33 +44,30 @@ class DriveHeadingModule{
    *
    *  @param   sample image to calculate centre lane value
    */
-  void setCentreLaneValue(cv::Mat);
-   
-/**
- *  @brief   Outputs the stored centre lane value
- *  @return  stored centre lane value
-*/
+  void setCentreLaneValue(const cv::Mat);
+
+ /**
+  *  @brief   Outputs the stored centre lane value
+  *  @return  stored centre lane value
+  */
   int getCentreLaneValue();
 
-/**
- * @brief   Calculates drive heading
- *
- * @param   point1 The 1st end point of line 1
- *
- * @param   point2 The 2nd end point of line 1
- *
- * @param   point3 The 1st end point of line 2
- *
- * @param   point4 The 2nd end point of line 2
- *
- * @return  Drive heading error
- */
-  float findDriveHeading(int, int, int, int);
+ /**
+  * @brief   Calculates drive heading
+  *
+  * @param   point1 The 1st end point of line 1
+  *
+  * @param   point2 The 2nd end point of line 1
+  *
+  * @param   point3 The 1st end point of line 2
+  *
+  * @param   point4 The 2nd end point of line 2
+  *
+  * @return  Drive heading error
+  */
+  float findDriveHeading(const int, const int, const int, const int);
 
  private:
   int centreLaneValue_;
-
 };
-
-#endif
-
+#endif  //  INCLUDE_DRIVEHEADINGMODULE_H_
